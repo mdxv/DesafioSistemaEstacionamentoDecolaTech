f2bd954e3bf198620595d9c80720c8a8134cc7ea
m0cb <mohan.elias@proton.me>
2024-01-05 16:20:21 -0300

    Desafio finalizado.

--- obj/Debug/net6.0/DesafioFundamentos.AssemblyInfo.cs
+++ obj/Debug/net6.0/DesafioFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ce4593669be29672195c9ed6d013e63376a61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7463be314a9a274f02d83f2280ce25211ba818")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
